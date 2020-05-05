dfd457d17e06a3657be1df346bc50f98aac3b221
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-05 08:30:55 +0530

    init and Show data without constructor

---
+++ init_show_data_method_1.php
@@ -0,0 +1,30 @@
+<?php
+	
+	class Student{
+
+		public $stud_id;
+		public $stud_name;
+		public $stud_email;
+		public $stud_mobile;
+
+		function init($id, $name, $email, $mobile){
+			  $this->stud_id = $id;
+			  $this->stud_name = $name;
+			  $this->stud_email = $email;
+			  $this->stud_mobile = $mobile; 
+		}
+
+		function show(){
+			  echo "$this->stud_id : $this->stud_name : {$this->stud_email} : {$this->stud_mobile} <br>";
+		}
+	}
+
+
+	$stud = new Student;
+	$stud->init(1001,"aaaaaa","example@gmail.com",9874563210);			// send data sequentially   > works
+	$stud->show();
+
+	$s2 = new Student;
+	$s2->init("Name",2002,123467890,"email@gmail.com");				// send data with mismatch sequence   > works
+	$s2->show();
+?>
