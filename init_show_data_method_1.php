9b9e45450f62907cf89ff2191e9eb410fd7955d0
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-05 08:38:43 +0530

    Update init_show_data_method_1.php

--- init_show_data_method_1.php
+++ init_show_data_method_1.php
@@ -8,14 +8,14 @@
 		public $stud_mobile;
 
 		function init($id, $name, $email, $mobile){
-			  $this->stud_id = $id;
-			  $this->stud_name = $name;
-			  $this->stud_email = $email;
-			  $this->stud_mobile = $mobile; 
+			$this->stud_id = $id;
+			$this->stud_name = $name;
+			$this->stud_email = $email;
+			$this->stud_mobile = $mobile; 
 		}
 
 		function show(){
-			  echo "$this->stud_id : $this->stud_name : {$this->stud_email} : {$this->stud_mobile} <br>";
+			echo "$this->stud_id : $this->stud_name : {$this->stud_email} : {$this->stud_mobile} <br>";
 		}
 	}
 
@@ -27,4 +27,13 @@
 	$s2 = new Student;
 	$s2->init("Name",2002,123467890,"email@gmail.com");				// send data with mismatch sequence   > works
 	$s2->show();
+
+	$s3 = new Student;
+	$s3->stud_id = 8888;
+	$s3->stud_name = "abc";
+	$s3->stud_email = "abc@gmail.com";
+	$s3->stud_mobile = 1111111111;
+	$s3->show();
+
+
 ?>
