119434a962c9e9a1b3f0a9c32481868ed6f5129a
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-06 11:53:30 +0530

    Update mysqli_pdo.php

--- MySQLi_PDO/mysqli_pdo.php
+++ MySQLi_PDO/mysqli_pdo.php
@@ -13,4 +13,20 @@
 		echo "Error to Connect";
 	}
 
+---------------------------------------------------------------------------------------------------------------
+	
+	
+	$data_source_name = "mysql: host = localhost; dbname=mydb";
+	$user = "root";
+	$password = "";
+	
+	$conn = new PDO($data_source_name,$user,$password);
+	if($conn){
+		echo "Connected";
+	}else{
+		echo "Error to Connect";
+	}
+	
+
+
 ?>
