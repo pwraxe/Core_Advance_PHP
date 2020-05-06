c5ba0b7fae59eea77906e390fd7df10bf3ad077b
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-06 10:55:26 +0530

    create table dynamically

---
+++ MySQLi_Object_Oriented/mysqli_oo_create_table.php
@@ -0,0 +1,22 @@
+<?php
+	$conn = new mysqli("localhost","root","","app_online_practice");
+	if($conn->connect_error){
+		die("Unable to Connect");
+	}
+	echo "Connected <hr>";
+
+	$sql = "CREATE TABLE xtable (
+			id INT AUTO_INCREMENT PRIMARY KEY,
+			name varchar(255),
+			email varchar(255),
+			mobile varchar(255) )";
+
+ 
+	if($conn->query($sql) === TRUE){
+		echo "Table Created";
+	}else{
+		echo "Error to create table";
+	}
+
+
+?>
