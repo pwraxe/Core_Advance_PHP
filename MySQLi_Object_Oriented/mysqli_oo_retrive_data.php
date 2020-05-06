7844bd84cf75d01899b3cf5ec48b81b86d84fa34
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-06 10:09:48 +0530

    Retrive MysqliData by Object oriented

---
+++ MySQLi_Object_Oriented/mysqli_oo_retrive_data.php
@@ -0,0 +1,24 @@
+<?php
+	$conn = new mysqli("localhost","root","","mydb");
+	if($conn->connect_error){
+		die("Unable to Connect");
+	}
+	echo "Connected <hr>";
+
+	$sql = "SELECT * FROM mytable";
+	$result = $conn->query($sql);
+
+	$total_rows_in_table = $result->num_rows;
+
+	if($total_rows_in_table > 0){
+		while ($row = $result->fetch_assoc()) {
+			echo " ID : ".$row['id']."<br>";
+			echo " Name : ".$row['name']."<br>";
+			echo " Email : ".$row['email']."<br>";
+			echo " Mobile : ".$row['mobile']."<br><br><br>";
+		}
+	}else{
+		echo "No Record Found :(";
+	}
+
+?>
