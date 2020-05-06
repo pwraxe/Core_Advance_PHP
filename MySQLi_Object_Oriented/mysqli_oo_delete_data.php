b1b945c918600af16861f013b44ad103cdcd3fa0
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-06 10:40:47 +0530

    delete data using mysqli object oriented

---
+++ MySQLi_Object_Oriented/mysqli_oo_delete_data.php
@@ -0,0 +1,15 @@
+<?php
+	$conn = new mysqli("localhost","root","","app_online_practice");
+	if($conn->connect_error){
+		die("Unable to Connect");
+	}
+	echo "Connected <hr>";
+
+	$sql = "DELETE FROM demo_table where id = 1003";
+ 
+	if($conn->query($sql) === TRUE){
+		echo "Record Deleted";
+	}else{
+		echo "Error to Delete Data";
+	}
+?>
