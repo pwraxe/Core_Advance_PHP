cef921924d089fba79a97c0947acf449ed0c9f0f
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-06 10:48:03 +0530

    create database Dynamically

---
+++ MySQLi_Object_Oriented/mysqli_oo_create_db.php
@@ -0,0 +1,17 @@
+<?php
+	$conn = new mysqli("localhost","root","","app_online_practice");
+	if($conn->connect_error){
+		die("Unable to Connect");
+	}
+	echo "Connected <hr>";
+
+	$sql = "CREATE DATABASE xdb";
+ 
+	if($conn->query($sql) === TRUE){
+		echo "Database Created";
+	}else{
+		echo "Error to Create Database";
+	}
+
+
+?>
