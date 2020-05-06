a409da31b0307eac7e876aae1da10a34d31067ce
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-06 21:23:10 +0530

    Retrive single and all data using mysqli_prep_stmnt

---
+++ MySQLi_Procedure_Prepare_Statement/RETRIVE_mysqli_proc_prep_statement.php
@@ -0,0 +1,52 @@
+======================================RETRIVE ALL DATA=====================================================
+
+<?php
+	$conn = mysqli_connect("localhost","root","","app_online_practice");
+	if(!$conn){
+		die("error to Connect".mysqli_connect_error());
+	}else{
+
+		$sql = "SELECT * FROM demo_table";
+
+		//Prepare Statement
+		$result = mysqli_prepare($conn,$sql);
+
+		// bind result set in variable
+		mysqli_stmt_bind_result($result, $id, $name, $email, $mobile);
+
+		// execute prepare statement
+		mysqli_stmt_execute($result);
+
+		while(mysqli_stmt_fetch($result)){
+			echo $id . " | ".$name." | ". $email ." | ".$mobile."<br/>";
+		}
+	}
+
+?>
+
+====================================RETRIVE SINGLE DATA=======================================================
+
+
+<?php
+	
+	$conn = mysqli_connect("localhost","root","","app_online_practice");
+	if(!$conn){
+		die("error to Connect".mysqli_connect_error());
+	}else{
+
+		$id = 1002;
+		$sql = "SELECT * FROM demo_table where id = ?";
+
+		$result = mysqli_prepare($conn,$sql);
+		
+		mysqli_stmt_bind_param($result,'i',$id);
+		mysqli_stmt_bind_result($result, $id, $name, $email, $mobile);
+
+		mysqli_stmt_execute($result);
+
+		while(mysqli_stmt_fetch($result)){
+			echo $id . " | ".$name." | ". $email ." | ".$mobile."<br/>";
+		}
+	}
+
+?>
