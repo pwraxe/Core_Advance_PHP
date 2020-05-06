0bfa351dc38af3bdd2f613275a902fa094b3f9c8
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-06 10:44:23 +0530

    update data by mysqli object oriented way

---
+++ MySQLi_Object_Oriented/mysqli_oo_update_data.php
@@ -0,0 +1,17 @@
+<?php
+	$conn = new mysqli("localhost","root","","app_online_practice");
+	if($conn->connect_error){
+		die("Unable to Connect");
+	}
+	echo "Connected <hr>";
+
+	$sql = "UPDATE demo_table SET name = 'John Doe', email = 'john@gmail.com', mobile='965412387' where id=13456 ";
+ 
+	if($conn->query($sql) === TRUE){
+		echo "Record Updated";
+	}else{
+		echo "Error to Update Data";
+	}
+
+
+?>
