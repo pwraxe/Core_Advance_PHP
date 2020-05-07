18301ecac9dc63b8494d1bc4bbf0dbd42f364873
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-07 10:23:36 +0530

    Insert Data in Database Using MySQLi OO PS

--- MySQLi_OO_Prepare_Statement/INSERT_Data_MySQLi_OO_PS.php
+++ MySQLi_OO_Prepare_Statement/INSERT_Data_MySQLi_OO_PS.php
@@ -27,3 +27,35 @@
 		
 	}
 ?>
+============================================= BETTER APPROCH ===========================================================
+
+<?php
+
+	$conn = new mysqli("localhost","root","","app_online_practice");
+	if($conn->connect_error){
+		die("Could not Connect to Database");
+	}else{
+		echo "Connected to Database<hr>";
+
+		$sql = "INSERT INTO demo_table (id, name, email, mobile) VALUES(?, ?, ?, ?)";
+
+		$result = $conn->prepare($sql);
+		if($result){
+			
+			$id = 1235;
+			$name = "Michel Johnson";
+			$email= "mj@gmail.com";
+			$mobile = 52554624875;
+
+			$result->bind_param('issi',$id,$name,$email,$mobile);
+
+			$result->execute();
+
+			// return 1 on success and return -1 on failuar
+			echo $result->affected_rows ." Inserted";	
+		}
+
+		$result->close();
+		$conn->close();
+	}
+?>
