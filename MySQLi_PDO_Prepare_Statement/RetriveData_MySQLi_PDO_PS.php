6ba2ddc5a30ab96058010a811e96f31a3a71e229
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-07 13:16:22 +0530

    PDO with Prep. Stmt , Retrive Data by Bind and unbind way

--- MySQLi_PDO_Prepare_Statement/RetriveData_MySQLi_PDO_PS.php
+++ MySQLi_PDO_Prepare_Statement/RetriveData_MySQLi_PDO_PS.php
@@ -1,3 +1,38 @@
+==================================== PDO Retrive Data without binding  , PDO Prepare Statement ========================================
+
+<?php
+
+	try{
+
+		$conn = new PDO("mysql: host = localhost; dbname = app_online_practice", "root", "");
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Connected...<hr>";
+
+		// select database  // if you are not write this then exception occure
+		($conn->prepare("use app_online_practice"))->execute();
+
+		$sql = "SELECT * FROM demo_table";
+
+		$result = $conn->prepare($sql);
+
+		$result->execute();
+
+		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+
+			echo " ID 	 : ".$row['id']."<br>";
+			echo " Name  : ".$row['name']."<br>";
+			echo " Email : ".$row['email']."<br>";
+			echo " Mobile : ".$row['mobile']."<br><br>";
+		}
+		unset($result);
+		$conn = null;
+	}catch(PDOException $e){ echo "Error : ".$e->getMessage(); }
+
+?>
+
+
+
+
 ====================================================PDO PrepStmt-> Retrive Data Using Bind Data by Column || index===============================
 
 <?php
