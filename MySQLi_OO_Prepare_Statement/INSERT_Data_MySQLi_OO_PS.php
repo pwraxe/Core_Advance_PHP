1f61f509edf81ceaaf7771af0a6fb886b2979028
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-07 10:16:07 +0530

    Create INSERT_Data_MySQLi_OO_PS.php

---
+++ MySQLi_OO_Prepare_Statement/INSERT_Data_MySQLi_OO_PS.php
@@ -0,0 +1,29 @@
+<?php
+
+	$conn = new mysqli("localhost","root","","app_online_practice");
+	if($conn->connect_error){
+		die("Could not Connect to Database");
+	}else{
+		echo "Connected to Database<hr>";
+
+		$sql = "INSERT INTO demo_table (id, name, email, mobile) VALUES(?, ?, ?, ?)";
+
+		$result = $conn->prepare($sql);
+
+		$id = 1234;
+		$name = "Michel Johnson";
+		$email= "mj@gmail.com";
+		$mobile = 52554624875;
+
+		$result->bind_param('issi',$id,$name,$email,$mobile);
+
+		$result->execute();
+
+		echo $result->affected_rows ." Inserted";	// return 1 on success and return -1 on failuar
+
+		$result->close();
+
+		$conn->close();
+		
+	}
+?>
