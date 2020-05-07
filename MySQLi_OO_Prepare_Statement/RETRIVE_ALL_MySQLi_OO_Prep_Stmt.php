e191fdd86e7a47402ebca5009a63e29753955745
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-07 09:27:17 +0530

    Retrive all Data with Mysqli Obj Orien. Prep Stmt

---
+++ MySQLi_OO_Prepare_Statement/RETRIVE_ALL_MySQLi_OO_Prep_Stmt.php
@@ -0,0 +1,19 @@
+<?php
+
+	$conn = new mysqli("localhost","root","","app_online_practice");
+	if($conn->connect_error){
+		die("Could not Connect to Database");
+	}else{
+		echo "Connected to Database<hr>";
+
+		$sql = "SELECT * FROM demo_table";
+		$result = $conn->prepare($sql);
+
+		$result->bind_result($id,$name, $email, $mobile);
+		$result->execute();
+
+		while($result->fetch()){
+			echo " ID : $id <br> Name : $name <br> Email : $email <br> Mobile : $mobile <br><br>";
+		}
+	}
+?>
