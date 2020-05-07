2980d449cc3461dae55c0ceea48a96f82f69de59
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-07 13:45:57 +0530

    Retrive Single row using PDO Prep. Stmt. , positional & named param

--- MySQLi_PDO_Prepare_Statement/RetriveSingleRow_PDO_PS.php
+++ MySQLi_PDO_Prepare_Statement/RetriveSingleRow_PDO_PS.php
@@ -1,5 +1,6 @@
-<?php
+===================================== Retrive Single Row by Positional Param using PDO Prepare Stmt====================================
 
+<?php
 	try{
 
 		$conn = new PDO("mysql: host = localhost; dbname = app_online_practice", "root", "");
@@ -25,6 +26,61 @@
 		unset($result);
 		$conn = null;
 	}catch(PDOException $e){ echo "Error : ".$e->getMessage(); }
+
+?>
+====================================== Retrive Single Row by Named Param using PDO Prepare Stmt====================================
+<?php
+
+	try{
+
+		$conn = new PDO("mysql: host = localhost; dbname = app_online_practice", "root", "");
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Connected...<hr>";
+
+		// select database  // if you are not write this then exception occure
+		($conn->prepare("use app_online_practice"))->execute();
+
+		$sql = "SELECT * FROM demo_table WHERE id = :id && mobile = :mobile";
+
+		$result = $conn->prepare($sql);
+
+		$id = 2001;
+		$mobile = 112534684; 
+
+		$result->bindParam(':id',$id);
+		$result->bindParam(':mobile',$mobile);
+
+
+		$result->execute();
+
+		$row = $result->fetch(PDO::FETCH_ASSOC);
+		echo " ID : ".$row['id']."<br> Name : ".$row['name']."<br> Email : ".$row['email']."<br> Mobile : ".$row['mobile']."<hr>";
+
+
+		unset($result);
+		$conn = null;
+	}catch(PDOException $e){ echo "Error : ".$e->getMessage(); }
+
+?>
+<?php
+/*
+
+
+if
+	$sql = "SELECT * FROM demo_table WHERE id = ?";
+then	
+	$result->bindParam(1,$id);
+
+---------------------------------------------------
+
+if
+	$sql = "SELECT * FROM demo_table WHERE id = ? && name = ?";
+then	
+	$result->bindParam(1,$id);
+	$result->bindParam(2,$name);
+----------------------------------------------------	
+
+*/
 
 ?>
 
