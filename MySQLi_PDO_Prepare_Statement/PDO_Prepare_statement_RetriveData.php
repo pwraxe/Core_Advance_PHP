0cb18bfb84f9128710f3b0332260f9119bf33a69
Akshay Sunil Pawar <31787030+pwraxe@users.noreply.github.com>
2020-05-07 12:41:55 +0530

    Retrive Data using PDO Prepare Statement

--- MySQLi_PDO_Prepare_Statement/PDO_Prepare_statement_RetriveData.php
+++ MySQLi_PDO_Prepare_Statement/PDO_Prepare_statement_RetriveData.php
@@ -6,11 +6,29 @@
 	// for 	PostgreSQL	database : The DSN prefix is --> pgsql: <--
 	// for 	PostgreSQL 	database : The DSN prefix is --> sqlite: <--
 
-	$conn = new PDO("mysql: host = localhost; dbname=mydb","root","");
-	if($conn){
-		echo "Connected";
-	}else{
-		echo "Error to Connect";
-	}
+	try{
 
+		$conn = new PDO("mysql: host = localhost; dbname = app_online_practice", "root", "");
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		echo "Connected...<hr>";
+
+		// select database  // if you are not write this then exception occure
+		($conn->prepare("use app_online_practice"))->execute();
+
+		$sql = "SELECT * FROM demo_table";
+
+		$result = $conn->prepare($sql);
+
+		$result->execute();
+
+		while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+
+			echo " ID 	 : ".$row['id']."<br>";
+			echo " Name  : ".$row['name']."<br>";
+			echo " Email : ".$row['email']."<br>";
+			echo " Mobile : ".$row['mobile']."<br><br>";
+		}
+		unset($result);
+		$conn = null;
+	}catch(PDOException $e){ echo "Error : ".$e->getMessage(); }
 ?>
